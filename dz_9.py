e8cea055d11142d2dacafd461055330fa9d334ca
malfiyy <rva2012@yandex.ru>
2022-11-13 17:43:40 +0300

    chore: test has been added

--- dz_9.py
+++ dz_9.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import pytest
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+@pytest.fixture
+def make_name_record():
+    def _make_customer_record(name):
+        return {"name": name}
+
+    return _make_customer_record
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def test_name_records(make_name_record):
+    name_1 = make_name_record("John")
+    name_2 = make_name_record("Name")
+    name_3 = make_name_record("123")
